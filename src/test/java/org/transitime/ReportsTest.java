503cd1521509023f75b8c928f03a3f1c0b9e1f4a
Grzegorz Patynek <grzegorz.patynek@goeuropa.eu>
2017-09-11 15:32:43 +0200

    pred accuracy csv

--- src/test/java/org/transitime/ReportsTest.java
+++ src/test/java/org/transitime/ReportsTest.java
@@ -10,12 +10,12 @@ package org.transitime;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -40,114 +40,134 @@ import org.testng.annotations.AfterTest;
 /**
  * @author Sean Óg Crudden
  * Test the reports.
- * 
+ *
  */
 public class ReportsTest {
 	private WebDriver driver;
 	private String baseUrl="http://127.0.0.1:8080/web";
-	
-	
+
+
 	/**
 	 * This looks at the last avl report and confirms vehicle report for today for at least one vehicle.
 	 * @throws InterruptedException
 	 */
 	@Test
 	public void lastAVLReport() throws InterruptedException {
-	
+
 		driver.get(baseUrl);
-		
+
 		String title = driver.getTitle();
-		
+
 		Assert.assertTrue(title.contains("Agencies"));
-		
+
 		driver.findElement(By.partialLinkText("Reports")).click();
-		
-		driver.findElement(By.partialLinkText("Last GPS Report by Vehicle")).click();										
-		
+
+		driver.findElement(By.partialLinkText("Last GPS Report by Vehicle")).click();
+
 		//TODO make this be a wait for table to load
 		Thread.sleep(5000);
-		
+
 		String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-		
+
 		Assert.assertTrue(driver.getPageSource().contains(date));
 	}
 	@Test
 	public void predictionAccuracyScatterPlot()
 	{
 		driver.get(baseUrl);
-		
+
 		String title = driver.getTitle();
-		
+
 		Assert.assertTrue(title.contains("Agencies"));
-		
+
 		driver.findElement(By.partialLinkText("Reports")).click();
-						
+
 		driver.findElement(By.partialLinkText("Prediction Accuracy Scatter Plot")).click();
-						
+
 		WebElement button = driver.findElement(By.id("submit"));
-		
-		button.submit();				
-		
+
+		button.submit();
+
 		Assert.assertTrue(driver.getPageSource().contains("Prediction Time"));
 	}
 	@Test
 	public void predictionAccuracyRangeChart()
 	{
 		driver.get(baseUrl);
-		
+
 		String title = driver.getTitle();
-		
+
 		Assert.assertTrue(title.contains("Agencies"));
-		
+
 		driver.findElement(By.partialLinkText("Reports")).click();
-						
+
 		driver.findElement(By.partialLinkText("Prediction Accuracy Range Chart")).click();
-						
+
 		WebElement button = driver.findElement(By.id("submit"));
-		
-		button.submit();				
-		
+
+		button.submit();
+
 		Assert.assertTrue(driver.getPageSource().contains("Prediction Length"));
 	}
 	@Test
 	public void scheduleAdherenceEarlyLate()
 	{
 		driver.get(baseUrl);
-		
+
 		String title = driver.getTitle();
-		
+
 		Assert.assertTrue(title.contains("Agencies"));
-		
+
 		driver.findElement(By.partialLinkText("Reports")).click();
-						
+
 		driver.findElement(By.partialLinkText("Schedule Adherence by how Early/Late")).click();
-						
+
 		WebElement button = driver.findElement(By.id("submit"));
-		
-		button.submit();				
-		
+
+		button.submit();
+
 		Assert.assertTrue(driver.getPageSource().contains("Minutes vehicle late (negative) or early (positive)"));
 	}
 	@Test
 	public void scheduleAdherenceByRoute()
 	{
 		driver.get(baseUrl);
-		
+
 		String title = driver.getTitle();
-		
+
 		Assert.assertTrue(title.contains("Agencies"));
-		
+
 		driver.findElement(By.partialLinkText("Reports")).click();
-						
+
 		driver.findElement(By.partialLinkText("Schedule Adherence by Route")).click();
-						
+
 		WebElement button = driver.findElement(By.id("submit"));
-		
-		button.submit();				
-		
+
+		button.submit();
+
 		Assert.assertTrue(driver.getPageSource().contains("On Time"));
 	}
-	
+
+	@Test
+	public void predictionAccuracyCSVDownload()
+	{
+		driver.get(baseUrl);
+
+		String title = driver.getTitle();
+
+		Assert.assertTrue(title.contains("Agencies"));
+
+		driver.findElement(By.partialLinkText("Reports")).click();
+
+		driver.findElement(By.partialLinkText("Prediction Accuracy CSV Download")).click();
+
+		WebElement button = driver.findElement(By.id("submit"));
+
+		button.submit();
+
+		Assert.assertTrue(driver.getPageSource().contains("pred_length_secs"));
+	}
+
 	@BeforeTest
 	public void beforeTest() {
 		driver = new ChromeDriver();
